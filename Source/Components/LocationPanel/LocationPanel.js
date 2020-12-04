0176db55356f9bc6691a87579c58ba092fdf7949
Rashad Balashov <Rashad.Balashov@gmail.com>
2020-12-04 18:31:34 -0500

    Pushing Updates - 12/4/2020

--- Source/Components/LocationPanel/LocationPanel.js
+++ Source/Components/LocationPanel/LocationPanel.js
@@ -1,106 +1,115 @@
-import React, { useRef } from 'react';
-import { Animated, View, Modal, StyleSheet, Text, KeyboardAvoidingView } from 'react-native';
-import WeatherIcon from '../WeatherIcon/WeatherIcon';
-import LocationPanelSubmitContainer from '../LocationPanelSubmit/LocationPanelSubmitContainer';
+import React, { useRef } from "react";
+import {
+  Animated,
+  View,
+  Modal,
+  StyleSheet,
+  Text,
+  KeyboardAvoidingView,
+} from "react-native";
+import WeatherIcon from "../WeatherIcon/WeatherIcon";
+import LocationPanelSubmitContainer from "../LocationPanelSubmit/LocationPanelSubmitContainer";
 
 export default function LocationPanel(props) {
+  const modalVisible = props.allLocations.length === 0 ? true : false;
+  const backgroundColor = props.style.backgroundColor;
+  const fontColor = props.style.color;
 
-    const modalVisible = props.allLocations.length === 0 ? true : false;
-    const backgroundColor = props.style.backgroundColor;
-    const fontColor = props.style.color;
-    
-    const FadeInView = (props) => {
-      const fadeAnim = useRef(new Animated.Value(0)).current  // Initial value for opacity: 0
-    
-      React.useEffect(() => {
-        Animated.timing(
-          fadeAnim,
-          {
-            toValue: 1,
-            duration: 1000,
-          }
-        ).start();
-      }, [])
-    
-      return (
-        <Animated.View                 // Special animatable View
-          style={{
-            ...props.style,
-            opacity: fadeAnim,         // Bind opacity to animated value
-          }}
-        >
-          {props.children}
-        </Animated.View>
-      );
-    }
-    const FadeInViewTwo = (props) => {
-      const fadeAnim = useRef(new Animated.Value(0)).current  // Initial value for opacity: 0
-    
-      React.useEffect(() => {
-        Animated.timing(
-          fadeAnim,
-          {
-            toValue: 1,
-            duration: 1300,
-          }
-        ).start();
-      }, [])
-    
-      return (
-        <Animated.View                 // Special animatable View
-          style={{
-            ...props.style,
-            opacity: fadeAnim,         // Bind opacity to animated value
-          }}
-        >
-          {props.children}
-        </Animated.View>
-      );
-    }
+  const FadeInView = (props) => {
+    const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
 
-    return(       
-        <Modal animationType="slide"
-                visible={modalVisible}
-                presentationStyle={"overFullScreen"}
-                transparent
-                statusBarTranslucent
-        >          
-        <KeyboardAvoidingView 
+    React.useEffect(() => {
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start();
+    }, []);
+
+    return (
+      <Animated.View // Special animatable View
+        style={{
+          ...props.style,
+          opacity: fadeAnim, // Bind opacity to animated value
+        }}
+      >
+        {props.children}
+      </Animated.View>
+    );
+  };
+  const FadeInViewTwo = (props) => {
+    const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
+
+    React.useEffect(() => {
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1300,
+        useNativeDriver: true,
+      }).start();
+    }, []);
+
+    return (
+      <Animated.View // Special animatable View
+        style={{
+          ...props.style,
+          opacity: fadeAnim, // Bind opacity to animated value
+        }}
+      >
+        {props.children}
+      </Animated.View>
+    );
+  };
+
+  return (
+    <Modal
+      animationType="slide"
+      visible={modalVisible}
+      presentationStyle={"overFullScreen"}
+      transparent
+      statusBarTranslucent
+    >
+      <KeyboardAvoidingView
         behavior={Platform.OS == "ios" ? "padding" : "height"}
-        style={{flex: 1}}
+        style={{ flex: 1 }}
+      >
+        <View
+          style={[styles.centeredView, { backgroundColor: backgroundColor }]}
         >
-          <View style={ [styles.centeredView, { backgroundColor: backgroundColor }]}>
-            <FadeInView>
-              <Text  style={ [styles.panelTitle, { color: fontColor }] }>
-                Welcome to SimpliWeather
-              </Text >
-            </FadeInView>
+          <FadeInView>
+            <Text style={[styles.panelTitle, { color: fontColor }]}>
+              Welcome to SimpliWeather
+            </Text>
+          </FadeInView>
 
-            <FadeInViewTwo>
-              <WeatherIcon icon={'clear'} isDay={false} style={{ width: 150, height: 150 }}/>
-            </FadeInViewTwo>
+          <FadeInViewTwo>
+            <WeatherIcon
+              icon={"clear"}
+              isDay={false}
+              style={{ width: 150, height: 150 }}
+            />
+          </FadeInViewTwo>
 
-            <FadeInViewTwo>
-              <LocationPanelSubmitContainer modalVisible={modalVisible} />
-            </FadeInViewTwo>
-          </View>
-          </KeyboardAvoidingView>
-        </Modal>
+          <FadeInViewTwo>
+            <LocationPanelSubmitContainer modalVisible={modalVisible} />
+          </FadeInViewTwo>
+        </View>
+      </KeyboardAvoidingView>
+    </Modal>
   );
 }
 
 const styles = StyleSheet.create({
-    panelTitle: {
-      fontSize: 46,
-      textAlign: "center",
-      paddingTop: 30
-    },
-    centeredView: {
-      flex: 1,
-      alignItems: "center",
-      flexDirection: 'column',
-      justifyContent: "space-between",
-      width: '100%',
-      paddingBottom: 10
-    }
+  panelTitle: {
+    fontSize: 46,
+    textAlign: "center",
+    paddingTop: 50,
+  },
+  centeredView: {
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingBottom: 30,
+  },
 });
--- Source/Components/LocationPanelSubmit/LocationPanelSubmit.js
+++ Source/Components/LocationPanelSubmit/LocationPanelSubmit.js
@@ -1,52 +1,55 @@
-import { Animated, View, StyleSheet, Text, TouchableOpacity, Dimensions } from 'react-native';
-import React, { useRef, useState } from 'react';
+import {
+  Animated,
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import React, { useRef, useState } from "react";
 
-import SimpliWeatherTextContainer from '../SimpliWeatherText/SimpliWeatherTextContainer';
-import TextInputSearch from '../TextInputSearch/TextInputSearch';
+import SimpliWeatherTextContainer from "../SimpliWeatherText/SimpliWeatherTextContainer";
+import TextInputSearch from "../TextInputSearch/TextInputSearch";
 
 export default function LocationPanelSubmit(props) {
+  const [textInputVisible, setTextInputVisible] = useState(false);
+  const [submitText, setText] = useState("");
 
-    const [textInputVisible, setTextInputVisible] = useState(false);
-    const [submitText, setText] = useState('');
+  const FadeInViewTwo = (props) => {
+    const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
 
-    const FadeInViewTwo = (props) => {
-        const fadeAnim = useRef(new Animated.Value(0)).current  // Initial value for opacity: 0
-      
-        React.useEffect(() => {
-          Animated.timing(
-            fadeAnim,
-            {
-              toValue: 1,
-              duration: 500,
-            }
-          ).start();
-        }, [])
-      
-        return (
-          <Animated.View                 // Special animatable View
-            style={{
-              ...props.style,
-              opacity: fadeAnim,         // Bind opacity to animated value
-            }}
-          >
-            {props.children}
-          </Animated.View>
-        );
-    }
-    const isZipCode = (submitText) => {
-      return !isNaN(parseInt(submitText));
-    }
+    React.useEffect(() => {
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    }, []);
 
-    if (textInputVisible === true) {
-      return (
+    return (
+      <Animated.View // Special animatable View
+        style={{
+          ...props.style,
+          opacity: fadeAnim, // Bind opacity to animated value
+        }}
+      >
+        {props.children}
+      </Animated.View>
+    );
+  };
+  const isZipCode = (submitText) => {
+    return !isNaN(parseInt(submitText));
+  };
+
+  if (textInputVisible === true) {
+    return (
       <View>
         <TextInputSearch submitText={submitText} setText={setText} />
 
-        <View style={ styles.currentLocationView }>
+        <View style={styles.currentLocationView}>
           <TouchableOpacity
             style={styles.continueButton}
-            onPress={ () => { 
-              
+            onPress={() => {
               if (isZipCode(submitText)) {
                 props.setLocationZip(parseInt(submitText));
                 if (!!props.closeModal) {
@@ -58,75 +61,87 @@ export default function LocationPanelSubmit(props) {
                   props.closeModal();
                 }
               }
-            
-            } }
-            >
+            }}
+          >
             <Text style={styles.continueText}>Continue</Text>
           </TouchableOpacity>
         </View>
 
         <FadeInViewTwo>
-            <TouchableOpacity onPress={ () => setTextInputVisible(!textInputVisible) } style={ styles.cityOrZipcodeContainer}><Text style={{ color: '#ED1C24', fontSize: 16 }}>Use your current location</Text></TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => setTextInputVisible(!textInputVisible)}
+            style={styles.cityOrZipcodeContainer}
+          >
+            <Text style={{ color: "#ED1C24", fontSize: 16 }}>
+              Use your current location
+            </Text>
+          </TouchableOpacity>
         </FadeInViewTwo>
       </View>
-      );
-    } else {
-      return (
-        <View>
-          <View style={ styles.currentLocationView }>
-            <SimpliWeatherTextContainer style= { styles.panelContext } >Allow SimpliWeather to use your current location</SimpliWeatherTextContainer>
-            
-            
-            <TouchableOpacity
-              style={styles.continueButton}
-              onPress={ () => {
-                props.getNewLocation();
-                if (!!props.closeModal) {
-                  props.closeModal();
-                }
-              }  }
-              >
-              <Text style={styles.continueText}>Continue</Text>
-            </TouchableOpacity>
-          </View>
+    );
+  } else {
+    return (
+      <View>
+        <View style={styles.currentLocationView}>
+          <SimpliWeatherTextContainer style={styles.panelContext}>
+            Allow SimpliWeather to use your current location
+          </SimpliWeatherTextContainer>
 
-          <FadeInViewTwo>
-              <TouchableOpacity onPress={ () => setTextInputVisible(!textInputVisible) } style={ styles.cityOrZipcodeContainer}><Text style={{ color: '#ED1C24', fontSize: 16 }}>Use City Name or ZipCode</Text></TouchableOpacity>
-          </FadeInViewTwo>
+          <TouchableOpacity
+            style={styles.continueButton}
+            onPress={() => {
+              props.getNewLocation();
+              if (!!props.closeModal) {
+                props.closeModal();
+              }
+            }}
+          >
+            <Text style={styles.continueText}>Continue</Text>
+          </TouchableOpacity>
         </View>
-      );
-    }
+
+        <FadeInViewTwo>
+          <TouchableOpacity
+            onPress={() => setTextInputVisible(!textInputVisible)}
+            style={styles.cityOrZipcodeContainer}
+          >
+            <Text style={{ color: "#ED1C24", fontSize: 16 }}>
+              Use City Name or ZipCode
+            </Text>
+          </TouchableOpacity>
+        </FadeInViewTwo>
+      </View>
+    );
   }
+}
 
-
-  
 const styles = StyleSheet.create({
-    panelContext: {
-      fontSize: 24,
-      textAlign: "center",
-      alignItems: "center",
-      width: 300,
-    },
-    currentLocationView: {
-      alignItems: "center",
-    },
-    continueButton:{
-      marginTop:10,
-      paddingTop:15,
-      paddingBottom:15,
-      backgroundColor:'#ED1C24',
-      borderRadius:15,
-      paddingLeft: 110,
-      paddingRight: 110,
-      width: '100%',
-    },
-    continueText:{
-      color:'#fff',
-      textAlign:'center',
-      fontSize: 22,
-    },
-    cityOrZipcodeContainer: {
-      marginTop: 10,
-      alignItems: "center",
-    },
+  panelContext: {
+    fontSize: 24,
+    textAlign: "center",
+    alignItems: "center",
+    width: 300,
+  },
+  currentLocationView: {
+    alignItems: "center",
+  },
+  continueButton: {
+    marginTop: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+    backgroundColor: "#ED1C24",
+    borderRadius: 15,
+    paddingLeft: 110,
+    paddingRight: 110,
+    width: "100%",
+  },
+  continueText: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 22,
+  },
+  cityOrZipcodeContainer: {
+    marginTop: 10,
+    alignItems: "center",
+  },
 });
